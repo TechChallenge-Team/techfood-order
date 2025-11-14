9ee9034f790a6b0c2510094a55b94de4142bf1c4
leandro oliveira cervantes <leandro_10_cervantes@hotmail.com>
2025-11-14 10:17:48 -0300

    Refactor OrderQueryProviderTests and OrderWorkflowTests to remove commented code and improve readability
    - Restored necessary using directives in OrderQueryProviderTests.
    - Removed commented-out code in OrderQueryProviderTests to enhance clarity.
    - Restored necessary using directives in OrderWorkflowTests.
    - Removed commented-out code in OrderWorkflowTests to enhance clarity.

--- tests/TechFood.Order.Domain.Tests/OrderTests.cs
+++ tests/TechFood.Order.Domain.Tests/OrderTests.cs
@@ -1,193 +1,193 @@
-// using TechFood.Order.Domain.Entities;
-// using TechFood.Order.Domain.Tests.Fixtures;
-// using TechFood.Shared.Domain.Exceptions;
+using TechFood.Order.Domain.Entities;
+using TechFood.Order.Domain.Tests.Fixtures;
+using TechFood.Shared.Domain.Exceptions;
 
-// namespace TechFood.Order.Domain.Tests
-// {
-//     public class OrderTests : IClassFixture<OrderFixture>
-//     {
-//         private readonly OrderFixture _orderFixture;
+namespace TechFood.Order.Domain.Tests
+{
+    public class OrderTests : IClassFixture<OrderFixture>
+    {
+        private readonly OrderFixture _orderFixture;
 
-//         public OrderTests(OrderFixture orderFixture)
-//         {
-//             _orderFixture = orderFixture;
-//         }
+        public OrderTests(OrderFixture orderFixture)
+        {
+            _orderFixture = orderFixture;
+        }
 
-//         [Fact(DisplayName = "Cannot add item to an order that is not in the Pending status.")]
-//         [Trait("Order", "Order Status")]
-//         public void ShoudThrowException_WhenAddingItemToOrderThatIsNotPendingStatus()
-//         {
-//             // Arrange
-//             var customerId = Guid.NewGuid();
-//             var order = _orderFixture.CreateValidOrder(customerId);
+        [Fact(DisplayName = "Cannot add item to an order that is not in the Pending status.")]
+        [Trait("Order", "Order Status")]
+        public void ShoudThrowException_WhenAddingItemToOrderThatIsNotPendingStatus()
+        {
+            // Arrange
+            var customerId = Guid.NewGuid();
+            var order = _orderFixture.CreateValidOrder(customerId);
 
-//             order.Cancel();
+            order.Cancel();
 
-//             var item = new OrderItem(
-//                 productId: Guid.NewGuid(),
-//                 quantity: 1,
-//                 unitPrice: 10);
+            var item = new OrderItem(
+                productId: Guid.NewGuid(),
+                quantity: 1,
+                unitPrice: 10);
 
-//             // Act
-//             var result = Assert.Throws<DomainException>(() => order.AddItem(item));
+            // Act
+            var result = Assert.Throws<DomainException>(() => order.AddItem(item));
 
-//             // Assert
-//             Assert.Equal(Domain.Resources.Exceptions.Order_CannotAddItemToNonPendingStatus, result.Message);
-//         }
+            // Assert
+            Assert.Equal(Domain.Resources.Exceptions.Order_CannotAddItemToNonPendingStatus, result.Message);
+        }
 
-//         [Fact(DisplayName = "Cannot remove item to an order that is not in the Pending status.")]
-//         [Trait("Order", "Order Status")]
-//         public void ShoudThrowException_WhenRemovingItemToOrderThatIsNotPendingStatus()
-//         {
-//             // Arrange
-//             var customerId = Guid.NewGuid();
-//             var order = _orderFixture.CreateValidOrder(customerId);
+        [Fact(DisplayName = "Cannot remove item to an order that is not in the Pending status.")]
+        [Trait("Order", "Order Status")]
+        public void ShoudThrowException_WhenRemovingItemToOrderThatIsNotPendingStatus()
+        {
+            // Arrange
+            var customerId = Guid.NewGuid();
+            var order = _orderFixture.CreateValidOrder(customerId);
 
-//             var item = new OrderItem(
-//                 productId: Guid.NewGuid(),
-//                 quantity: 1,
-//                 unitPrice: 10);
+            var item = new OrderItem(
+                productId: Guid.NewGuid(),
+                quantity: 1,
+                unitPrice: 10);
 
-//             order.AddItem(item);
+            order.AddItem(item);
 
-//             order.Cancel();
+            order.Cancel();
 
-//             // Act
-//             var result = Assert.Throws<DomainException>(() => order.RemoveItem(item.Id));
+            // Act
+            var result = Assert.Throws<DomainException>(() => order.RemoveItem(item.Id));
 
-//             // Assert
-//             Assert.Equal(Domain.Resources.Exceptions.Order_CannotRemoveItemToNonPendingStatus, result.Message);
-//         }
+            // Assert
+            Assert.Equal(Domain.Resources.Exceptions.Order_CannotRemoveItemToNonPendingStatus, result.Message);
+        }
 
-//         [Fact(DisplayName = "Validate Receive when Order is Pending")]
-//         [Trait("Order", "Order Status")]
-//         public void ShoudThrowException_WhenReceiveToOrderThatIsNotPendingStatus()
-//         {
-//             // Arrange
-//             var customerId = Guid.NewGuid();
-//             var order = _orderFixture.CreateValidOrder(customerId);
+        [Fact(DisplayName = "Validate Receive when Order is Pending")]
+        [Trait("Order", "Order Status")]
+        public void ShoudThrowException_WhenReceiveToOrderThatIsNotPendingStatus()
+        {
+            // Arrange
+            var customerId = Guid.NewGuid();
+            var order = _orderFixture.CreateValidOrder(customerId);
 
-//             order.Cancel();
+            order.Cancel();
 
-//             // Act
-//             var result = Assert.Throws<DomainException>(order.Receive);
+            // Act
+            var result = Assert.Throws<DomainException>(order.Receive);
 
-//             // Assert
-//             Assert.Equal(Domain.Resources.Exceptions.Order_CannotReceiveToNonPendingStatus, result.Message);
-//         }
+            // Assert
+            Assert.Equal(Domain.Resources.Exceptions.Order_CannotReceiveToNonPendingStatus, result.Message);
+        }
 
-//         [Fact(DisplayName = "Validate Discount Application when Order is Pending")]
-//         [Trait("Order", "Order Status")]
-//         public void ShoudThrowException_WhenApplyingDiscountToOrderThatIsNotPendingStatus()
-//         {
-//             // Arrange
-//             var customerId = Guid.NewGuid();
-//             var order = _orderFixture.CreateValidOrder(customerId);
+        [Fact(DisplayName = "Validate Discount Application when Order is Pending")]
+        [Trait("Order", "Order Status")]
+        public void ShoudThrowException_WhenApplyingDiscountToOrderThatIsNotPendingStatus()
+        {
+            // Arrange
+            var customerId = Guid.NewGuid();
+            var order = _orderFixture.CreateValidOrder(customerId);
 
-//             order.Cancel();
+            order.Cancel();
 
-//             // Act
-//             var result = Assert.Throws<DomainException>(() => order.ApplyDiscount(10));
+            // Act
+            var result = Assert.Throws<DomainException>(() => order.ApplyDiscount(10));
 
-//             // Assert
-//             Assert.Equal(Domain.Resources.Exceptions.Order_CannotApplyDiscountToNonPendingStatus, result.Message);
-//         }
+            // Assert
+            Assert.Equal(Domain.Resources.Exceptions.Order_CannotApplyDiscountToNonPendingStatus, result.Message);
+        }
 
-//         [Fact(DisplayName = "Validate Prepare when Order is Received")]
-//         [Trait("Order", "Order Status")]
-//         public void ShoudThrowException_WhenPrepareOrderThatIsNotReceivedStatus()
-//         {
-//             // Arrange
-//             var customerId = Guid.NewGuid();
-//             var order = _orderFixture.CreateValidOrder(customerId);
+        [Fact(DisplayName = "Validate Prepare when Order is Received")]
+        [Trait("Order", "Order Status")]
+        public void ShoudThrowException_WhenPrepareOrderThatIsNotReceivedStatus()
+        {
+            // Arrange
+            var customerId = Guid.NewGuid();
+            var order = _orderFixture.CreateValidOrder(customerId);
 
-//             order.Cancel();
+            order.Cancel();
 
-//             // Act
-//             var result = Assert.Throws<DomainException>(order.Receive);
+            // Act
+            var result = Assert.Throws<DomainException>(order.Receive);
 
-//             // Assert
-//             Assert.Equal(Domain.Resources.Exceptions.Order_CannotReceiveToNonPendingStatus, result.Message);
-//         }
+            // Assert
+            Assert.Equal(Domain.Resources.Exceptions.Order_CannotReceiveToNonPendingStatus, result.Message);
+        }
 
-//         [Fact(DisplayName = "Validate Ready when Order is InPreparation")]
-//         [Trait("Order", "Order Status")]
-//         public void ShoudThrowException_WhenReadyOrderThatIsNotInPreparationStatus()
-//         {
-//             // Arrange
-//             var customerId = Guid.NewGuid();
-//             var order = _orderFixture.CreateValidOrder(customerId);
+        [Fact(DisplayName = "Validate Ready when Order is InPreparation")]
+        [Trait("Order", "Order Status")]
+        public void ShoudThrowException_WhenReadyOrderThatIsNotInPreparationStatus()
+        {
+            // Arrange
+            var customerId = Guid.NewGuid();
+            var order = _orderFixture.CreateValidOrder(customerId);
 
-//             order.Cancel();
+            order.Cancel();
 
-//             // Act
-//             var result = Assert.Throws<DomainException>(order.Prepare);
+            // Act
+            var result = Assert.Throws<DomainException>(order.Prepare);
 
-//             // Assert
-//             Assert.Equal(Domain.Resources.Exceptions.Order_CannotPrepareToNonReceivedStatus, result.Message);
-//         }
+            // Assert
+            Assert.Equal(Domain.Resources.Exceptions.Order_CannotPrepareToNonReceivedStatus, result.Message);
+        }
 
-//         [Fact(DisplayName = "Validate Deliver when Order is Ready")]
-//         [Trait("Order", "Order Status")]
-//         public void ShoudThrowException_WhenDeliverOrderThatIsNotReadyStatus()
-//         {
-//             // Arrange
-//             var customerId = Guid.NewGuid();
-//             var order = _orderFixture.CreateValidOrder(customerId);
+        [Fact(DisplayName = "Validate Deliver when Order is Ready")]
+        [Trait("Order", "Order Status")]
+        public void ShoudThrowException_WhenDeliverOrderThatIsNotReadyStatus()
+        {
+            // Arrange
+            var customerId = Guid.NewGuid();
+            var order = _orderFixture.CreateValidOrder(customerId);
 
-//             order.Cancel();
+            order.Cancel();
 
-//             // Act
-//             var result = Assert.Throws<DomainException>(order.Deliver);
+            // Act
+            var result = Assert.Throws<DomainException>(order.Deliver);
 
-//             // Assert
-//             Assert.Equal(Domain.Resources.Exceptions.Order_CannotDeliverToNonReadyStatus, result.Message);
-//         }
+            // Assert
+            Assert.Equal(Domain.Resources.Exceptions.Order_CannotDeliverToNonReadyStatus, result.Message);
+        }
 
-//         [Fact(DisplayName = "Validate Amount Calculation in Order")]
-//         [Trait("Order", "Calculation")]
-//         public void ShoudThrowException_WhenCalculatingAmountIsNotCorrect()
-//         {
-//             // Arrange
-//             var customerId = Guid.NewGuid();
-//             var order = _orderFixture.CreateValidOrder(customerId);
+        [Fact(DisplayName = "Validate Amount Calculation in Order")]
+        [Trait("Order", "Calculation")]
+        public void ShoudThrowException_WhenCalculatingAmountIsNotCorrect()
+        {
+            // Arrange
+            var customerId = Guid.NewGuid();
+            var order = _orderFixture.CreateValidOrder(customerId);
 
-//             var item = new OrderItem(
-//                 productId: Guid.NewGuid(),
-//                 quantity: 7,
-//                 unitPrice: 10.42m);
+            var item = new OrderItem(
+                productId: Guid.NewGuid(),
+                quantity: 7,
+                unitPrice: 10.42m);
 
-//             order.AddItem(item);
-//             order.AddItem(item);
-//             order.AddItem(item);
-//             order.AddItem(item);
+            order.AddItem(item);
+            order.AddItem(item);
+            order.AddItem(item);
+            order.AddItem(item);
 
-//             // Act
+            // Act
 
-//             // Assert
-//             Assert.Equal(7 * 10.42m * 4, order.Amount);
-//         }
+            // Assert
+            Assert.Equal(7 * 10.42m * 4, order.Amount);
+        }
 
-//         [Fact(DisplayName = "Validate Discount Calculation in Order")]
-//         [Trait("Order", "Calculation")]
-//         public void ShoudThrowException_WhenCalculatingDiscountIsNotCorrect()
-//         {
-//             // Arrange
-//             var customerId = Guid.NewGuid();
-//             var order = _orderFixture.CreateValidOrder(customerId);
+        [Fact(DisplayName = "Validate Discount Calculation in Order")]
+        [Trait("Order", "Calculation")]
+        public void ShoudThrowException_WhenCalculatingDiscountIsNotCorrect()
+        {
+            // Arrange
+            var customerId = Guid.NewGuid();
+            var order = _orderFixture.CreateValidOrder(customerId);
 
-//             var item = new OrderItem(
-//                 productId: Guid.NewGuid(),
-//                 quantity: 7,
-//                 unitPrice: 10.42m);
+            var item = new OrderItem(
+                productId: Guid.NewGuid(),
+                quantity: 7,
+                unitPrice: 10.42m);
 
-//             order.AddItem(item);
-//             order.ApplyDiscount(9.76m);
+            order.AddItem(item);
+            order.ApplyDiscount(9.76m);
 
-//             // Act
+            // Act
 
-//             // Assert
-//             Assert.Equal((7 * 10.42m) - 9.76m, order.Amount);
-//         }
-//     }
-// }
+            // Assert
+            Assert.Equal((7 * 10.42m) - 9.76m, order.Amount);
+        }
+    }
+}
